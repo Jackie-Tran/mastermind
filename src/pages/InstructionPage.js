dab203451e44c5c843f50321e4dee111e8c2ff42
Jackie-Tran <jtran2009a@gmail.com>
2019-12-29 21:32:42 -0500

    Finished page routing

--- src/pages/InstructionPage.js
+++ src/pages/InstructionPage.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react'
 
+import { Link } from 'react-router-dom'
+
 export default class InstructionPage extends Component {
     render() {
         return (
             <div>
                 <h1>This is the instruction page</h1>
+                <Link to="/">Home</Link>
             </div>
         )
     }
--- src/pages/MenuPage.js
+++ src/pages/MenuPage.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
 
 export default class MenuPage extends Component {
     render() {
         return (
             <div>
                 <h1>This is the menu page</h1>
+                <Link to="/instruction">Instruction Page</Link>
+                <br/>
+                <Link to="/game">Game Page</Link>
             </div>
         )
     }
