bfe56c35cb96d2de9978b4d1333e392dec8a3dde
Jackie-Tran <jtran2009a@gmail.com>
2020-01-03 18:12:31 -0500

    Moved MenuPage style to css file

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import './App.css';
 
-import { BrowserRouter as Router, Route, Switch, Link, Redirect } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom'
 
 // Pages 
 import MenuPage from './pages/MenuPage'
-import InstructionPage from './pages/InstructionPage'
+import RulesPage from './pages/RulesPage'
 import GamePage from './pages/GamePage'
 import NotFoundPage from './pages/404Page'
 
@@ -14,7 +14,7 @@ function App() {
     <Router>
       <Switch>
         <Route exact path="/" component={MenuPage} />
-        <Route exact path="/instruction" component={InstructionPage} />
+        <Route exact path="/rules" component={RulesPage} />
         <Route exact path="/game" component={GamePage} />
         <Route exact path="/404" component={NotFoundPage} />
         <Redirect to="/404" />
--- src/pages/MenuPage.js
+++ src/pages/MenuPage.js
@@ -1,58 +1,33 @@
-import React, { Component } from 'react'
-import { Link } from 'react-router-dom'
-import circles from '../images/menuColors.png'
+import React, { Component } from 'react';
+import './MenuPageStyle.css'
+import { Link } from 'react-router-dom';
+import circles from '../images/menuColors.png';
+
 
 export default class MenuPage extends Component {
     render() {
         return (
-            <div style={pageStyle}>
-                <div style={titleStyle}>
-                    <h1 style={title}>m a s t e r m i n d</h1>
-                    <img src={circles} style={imgStyle} />
-                    <div style={creditStyle}>
+            <div className="Page">
+                <div className="Title">
+                    <h1 className="TitleText">m a s t e r m i n d</h1>
+                    <img className="Image" src={circles} alt='Circles' />
+                    <div className="Credit">
                         <h4>Game Remade By</h4>
                         <h5>Jackie Tran | Derek Khau | Michael Lam</h5>
                     </div>
                 </div>
 
-                <div className='menuButtons'>
-                    <Link to="/instruction">Instruction Page</Link>
-                    <br />
-                    <Link to="/game">Game Page</Link>
+                <div className="Buttons">
+                    <div className="button" id="button-3">
+                        <div id="circle"></div>
+                        <Link to="/game">Play</Link>
+                    </div>
+                    <div className="button" id="button-3">
+                        <div id="circle"></div>
+                        <Link to="/rules">Rules</Link>
+                    </div>
                 </div>
             </div>
         )
     }
-}
-
-const pageStyle = {
-    textAlign: 'center',
-    backgroundColor: '#3b5295',
-    minHeight: '100vh'
-}
-
-const titleStyle = {
-    color: '#d5d6da',
-    textAlign: 'center',
-    position: 'absolute',
-    height: '200px',
-    width: '400px',
-    margin: '-100px 0 0 -200px',
-    top: '50%',
-    left: '50%',
-}
-
-const imgStyle = {
-    display: 'block',
-    marginLeft: 'auto',
-    marginRight: 'auto', 
-    width: '85%'
-}
-
-const creditStyle = {
-    textAlign: 'right'
-}
-
-const title = {
-    fontSize: '300%'
 }
