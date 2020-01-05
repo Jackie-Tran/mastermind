c9fa02e700d4bbec90d4e3b08c6dfe68c3959be5
Jackie-Tran <jtran2009a@gmail.com>
2020-01-04 20:15:38 -0500

    Added button to return home

--- src/pages/404Page.js
+++ src/pages/404Page.js
@@ -1,14 +1,20 @@
 import React, { Component } from 'react'
 
 import './404PageStyle.css'
+import { Link } from 'react-router-dom'
 
 export default class NotFoundPage extends Component {
     render() {
         return (
             <div className='content'>
                 <h1>Page not found</h1>
-                <p>Sorry but the page you requested could not be found. 
-                    Please go back to the homepage or post an issue on the github repo.</p>
+                <p>Sorry but the page you requested could not be found.
+                    Please go back to the homepage or post an issue on the <a id="git" href="https://github.com/Jackie-Tran/mastermind" target="_blank" rel="noopener noreferrer">github repo</a>.</p>
+
+                <div class="button" id="button-2">
+                    <div id="slide"></div>
+                    <Link to="/">Home</Link>
+                </div>
             </div>
         )
     }
